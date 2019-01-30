afa9e657dad7133ee39cea96ab2047123aca71f9
Anton  Agency <info@anton.agency>
2019-01-29 22:51:51 -0400

    Blog submenu colums

--- header-blog.php
+++ header-blog.php
@@ -4,27 +4,35 @@
 
 <div class="main-subnavbar">
 <div class="container">
-		<a class="navbar-brand" href="<?php echo site_url(); ?>"><img src="<?php echo esc_attr(get_option('logo-001')); ?>" alt="<?php the_title()?>"></a>
-        <nav class="navbar navbar-expand-md navbar-light" role="navigation">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs4navbar" aria-controls="bs4navbar" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-			<?php
-			wp_nav_menu([
-				'menu'            => 'Blog Menú',
-				'theme_location'  => 'menu-2',
-				'container'       => 'div',
-				'container_id'    => 'bs4navbar',
-				'container_class' => 'collapse navbar-collapse justify-content-start',
-				'menu_id'         => false,
-				'menu_class'      => 'navbar-nav mb-auto',
-				'depth'           => 2,
-				'fallback_cb'     => 'bs4navwalker::fallback',
-				'walker'          => new bs4navwalker()
-			]);
-			?>
-        </nav>
-		<?php get_search_form()?> 
+	<div class="row">
+		<div class="col">
+			<a class="navbar-brand" href="<?php echo site_url(); ?>"><img src="<?php echo esc_attr(get_option('logo-001')); ?>" alt="<?php the_title()?>"></a>
+		</div>
+		<div class="col">
+			<nav class="navbar navbar-expand-md navbar-light" role="navigation">
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs4navbar" aria-controls="bs4navbar" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<?php
+				wp_nav_menu([
+					'menu'            => 'Blog Menú',
+					'theme_location'  => 'menu-2',
+					'container'       => 'div',
+					'container_id'    => 'bs4navbar',
+					'container_class' => 'collapse navbar-collapse justify-content-start',
+					'menu_id'         => false,
+					'menu_class'      => 'navbar-nav mb-auto',
+					'depth'           => 2,
+					'fallback_cb'     => 'bs4navwalker::fallback',
+					'walker'          => new bs4navwalker()
+				]);
+				?>
+			</nav>
+		</div>
+		<div class="col">
+			<?php get_search_form()?> 
+		</div>
+	</div>
     </div>
 </div>
 
