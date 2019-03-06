683e1ec4718782f92fa8805b55c17dde9b8a0822
Starfish <starfish.host@gmail.com>
2019-03-06 03:35:16 -0400

    Se personalizo el diseño de la página de categorías

--- archive.php
+++ archive.php
@@ -13,35 +13,45 @@ get_header('blog');
 	<div id="primary" class="content-area">
 		<div class="container pb-5">
 			
-			<header class="mb-5 archive-header">
-				<div class="row">
-					<div class="col">
-						<?php
-							the_archive_description( '<div class="archive-description">', '</div>' );
-						?>
-					</div>
-				</div>
-			</header>
 			
 			<div class="row">
-				<?php
-				/* Start the Loop */
+				<div class="col-md-4">
+					<?php get_sidebar();?>
+				</div>
+				<div class="col-md-8">
+					<header class="mb-5 archive-header">
+						<div class="row">
+							<div class="col">
+								<span><mark>Contenido de:</mark></span>
+								<h1 class="mb-3"><?php single_cat_title(); ?></h1>
+								<?php
+									the_archive_description( '<div class="archive-description">', '</div>' );
+								?>
+							</div>
+						</div>
+					</header>
+					<div class="row">
+						<?php
+						/* Start the Loop */
 
-				if ( have_posts() ) : 
-					while ( have_posts() ) :
-						the_post();
+						if ( have_posts() ) : 
+							while ( have_posts() ) :
+								the_post();
 
-						/*
-						* Include the Post-Type-specific template for the content.
-						* If you want to override this in a child theme, then include a file
-						* called content-___.php (where ___ is the Post Type name) and that will be used instead.
-						*/
-						get_template_part( 'template-parts/post-medium', get_post_type() );
+								/*
+								* Include the Post-Type-specific template for the content.
+								* If you want to override this in a child theme, then include a file
+								* called content-___.php (where ___ is the Post Type name) and that will be used instead.
+								*/
+								get_template_part( 'template-parts/post-medium', get_post_type() );
 
-					endwhile;
+							endwhile;
 
-			endif;
-			?>
+					endif;
+					?>
+					</div>
+				</div>
+
 			</div>
 		</div>
 	</div><!-- #primary -->
